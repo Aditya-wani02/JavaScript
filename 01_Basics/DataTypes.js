fff1f36b4c6dd4c4b759d95434a3d85c53b656e5
Aditya Wani <114596489+Aditya-wani02@users.noreply.github.com>
2023-08-12 14:35:34 +0000

    DataTypes

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; 
+// Treat all JS code as newer version
+
+// alert(3+3) 
+// we are using nodejs not browser
+
+console.log(3+3)
+// code readability should be high
+
+let name = "Aditya"
+let age = 20
+let isLoggedIn = true
+
+// number -=> 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "Aditya");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
