9a9e05bd294b4af623b53f6f47b1f2e078a86680
Aditya Wani <114596489+Aditya-wani02@users.noreply.github.com>
2023-08-12 16:19:46 +0000

    Opertions

---
+++ 01_Basics/Operations.js
@@ -0,0 +1,33 @@
+let val = 3
+let negVal = -val
+
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+
+let str1 = "Hello"
+let str2  = " Aditya"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1 , num2 , num3 
+// num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter ++;
+console.log(gameCounter);
