e40afbe3fb0097b5d6f08218166730473f2979b0
Aditya Wani <114596489+Aditya-wani02@users.noreply.github.com>
2023-08-12 14:48:17 +0000

    Conversion

---
+++ 01_Basics/ConversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33as"
+let score = null
+
+
+
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 0
+
+let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolIsLoggedIn);
+
+// true => 1 ; false => 0
+// "" => false
+// "Aditya" => true
+
+let someNumber = 44
+let stringNumber = String(someNumber) 
+console.log(stringNumber);
+console.log(typeof stringNumber);
