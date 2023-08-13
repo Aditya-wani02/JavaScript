56bc35bd2d9f7db43dd450082a340a37ea888cc1
Aditya Wani <114596489+Aditya-wani02@users.noreply.github.com>
2023-08-13 06:26:43 +0000

    Summary

---
+++ 01_Basics/Comparison.js
@@ -0,0 +1,30 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02"  > 1);
+
+
+// Avoid this type of comparison
+
+// console.log(null > 0);  // false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
+// === strict check also check datatypes
+
+console.log("2" === 2);
+
+
+
---
+++ 01_Basics/Summary.js
@@ -0,0 +1,67 @@
+// categorization is based on how data is present in the memory and how the data is accessed
+
+
+// Dynamically typed Language
+
+// primitive and non-primitive
+
+// Primitive
+
+// 7 types :String, Number , Boolean , null ,undefined , Symbol , BigInt
+ 
+const score = 100
+const scoreVal = 100.4
+
+const isLoggedin  =true
+const outsideTemp = null
+let Email ;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId );
+
+const BigNumber = 65416846465165467684n
+console.log("BigInt " + typeof BigNumber);
+
+
+// Reference (non-primitive)
+
+// Arrays , Objects, Functions  --> return function
+
+const heros = ["Shaktimaan" , "Naagraj"];
+
+let myObj = {
+    name: "Aditya",
+    age : 20
+}
+
+// console.log(myObj);
+
+const myFun =  function(){
+    console.log("Hello World");
+}
+// myFun()
+
+console.log(typeof Email)
+console.log(typeof outsideTemp)
+console.log( typeof myObj);
+console.log(typeof myFun);
+console.log(typeof heros);
+
+
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
