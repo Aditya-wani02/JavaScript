28695f714020f0025ad591207fcb61ca21cf4bef
Aditya Wani <114596489+Aditya-wani02@users.noreply.github.com>
2023-08-18 19:36:30 +0000

    scopes

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -39,4 +39,41 @@ function loginUserMessage(username = "Sam"){
 }
 
 // console.log(loginUserMessage("Aditya"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+//                           rest operator
+function calculateCartPrice(val1,val2,...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice(200,300,400,2000));
+
+
+// passing object in function
+
+const user = {
+    name: "Aditya",
+    price : 100
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.name} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    name: "sam",
+    price : 100
+}
+)
+
+
+const myNewArray = [200,300,400,500]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+console.log(returnSecondValue(myNewArray));
