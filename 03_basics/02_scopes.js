0bcd1db0c4866cb77c91b29cdc4aff0fcb735555
Aditya Wani <114596489+Aditya-wani02@users.noreply.github.com>
2023-08-18 19:50:23 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,12 +6,48 @@ if(true){
     let a =10
     const b =30
     //  c = 30
-console.log("INNER" , a);
+// console.log("INNER: " , a);
     
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+const username = "Aditya"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+// one()
+
+if(true){
+    const name = "Aditya"
+    if(name === "Aditya"){
+        const website = " github"
+        console.log(name + website);
+    }
+    // console.log(website);
+}
+// console.log(name);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num) {
+    return num +1
+}
+
+
+addTwo(5) //Cannot access 'addTwo' before initialization
+const addTwo = function(num) {
+    return num +2
+}
