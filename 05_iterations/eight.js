e7b5fa553bb8c0a06e6c360552ecaf5354d58477
Aditya Wani <114596489+Aditya-wani02@users.noreply.github.com>
2023-08-20 18:04:53 +0000

    iterations

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,6 +1,34 @@
 const myNum = [1,2,3]
 
-const mytotal = myNum.reduce(funtion(acc , currval){
+// const mytotal = myNum.reduce(function(acc , currval){
+//     console.log(acc,currval);
 
-    return acc +currval
-}) 
+//     return acc +currval
+// },0)
+
+const mytotal = myNum.reduce((acc,curr) => acc+curr,0)
+
+console.log(mytotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const pricetopay = shoppingCart.reduce((acc,item)=>acc+item.price,0)
+
+console.log(pricetopay);
